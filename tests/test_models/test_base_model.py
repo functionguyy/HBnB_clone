5759c213a8a7bac2878e31daba0f2875297aebd2
Omar Jammeh <orms45@gmail.com>
2023-05-10 00:38:59 +0000

    Base model test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,14 @@
+#!/usr/bin/python3
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
-    """Base mode test suite"""
+    """Base model test suite"""
 
     test_instance_id(self):
         """
-        Test that a BaseModel instance has a unique ID when Created
+            Test that a BaseModel instance has a unique ID when Created
         """
         bm1 = BaseModel()
         bm2 = BaseModel()
@@ -15,8 +16,8 @@ class TestBaseModel(unittest.TestCase):
 
     test_created_at_assigned_at_current_time(self):
         """
-        Test that created_at is assigned the current datetime
-        when an instance is created
+            Test that created_at is assigned the current datetime
+            when an instance is created
         """
         bm1 = BaseModel()
         created_at = bm1.created_at
@@ -25,8 +26,8 @@ class TestBaseModel(unittest.TestCase):
 
     test_to_dict_returns_all_attr_values(self):
         """
-        Test that to_dict method returns a dictionary containing
-        all keys values of dict of the instance
+            Test that to_dict method returns a dictionary containing
+            all keys values of dict of the instance
         """
         bm1 = BaseModel()
         bm1.id = "123"
@@ -45,7 +46,7 @@ class TestBaseModel(unittest.TestCase):
 
     test_to_dict_contains_class_key(self):
         """
-        Test that dictionary contains '__class__' key
+            Test that dictionary contains __class__ key
         """
         bm = BaseModel()
 
@@ -56,8 +57,8 @@ class TestBaseModel(unittest.TestCase):
 
     test_to_dict_iso_format(self):
         """
-        Test that the to_dict method converts created_at and
-        update_at to string object in ISO format.
+            Test that the to_dict method converts created_at and
+            update_at to string object in ISO format.
         """
         bm = BaseModel()
 
