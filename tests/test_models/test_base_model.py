6444acd1e25122547cd612db750d08ff19dc5d83
Omar Jammeh <orms45@gmail.com>
2023-05-10 01:01:49 +0000

    Doc updates

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,27 +3,33 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 
+
 class TestBaseModel(unittest.TestCase):
     """Base model test suite"""
 
-    test_instance_id(self):
+    def test_instance_id(self):
         """
-        Test that a Base model instance has a unique id when created
+            Test that a BaseModel instance has a unique ID when Created
         """
         bm1 = BaseModel()
         bm2 = BaseModel()
         self.assertNotEqual(bm1.id, bm2.id)
 
-    test_created_at_assigned_at_current_time(self):
+    def test_created_at_assigned_at_current_time(self):
         """
-        Test that created_at is assigned the current datetime
+            Test that created_at is assigned the current datetime
+            when an instance is created
         """
         bm1 = BaseModel()
         created_at = bm1.created_at
         current_time = datetime.now
         self.assertEqual(created_at, current_time)
 
-    test_to_dict_returns_all_attr_values(self):
+    def test_to_dict_returns_all_attr_values(self):
+        """
+            Test that to_dict method returns a dictionary containing
+            all keys values of dict of the instance
+        """
         bm1 = BaseModel()
         bm1.id = "123"
         bm1.created_at = "2022-05-09T14:30:00.000000"
@@ -33,13 +39,16 @@ class TestBaseModel(unittest.TestCase):
 
         bm_dict = bm1.to_dict()
 
-        assertEqual(bm_dict['id'], "123")
-        assertEqual(bm_dict['created_at'], "2022-05-09T14:30:00.000000")
-        assertEqual(bm_dict['updated'], "2022-05-09T14:30:00.000000")
-        assertEqual(bm_dict['name'], "example")
-        assertEqual(bm_dict['number'], 42)
+        self.assertEqual(bm_dict['id'], "123")
+        self.assertEqual(bm_dict['created_at'], "2022-05-09T14:30:00.000000")
+        self.assertEqual(bm_dict['updated'], "2022-05-09T14:30:00.000000")
+        self.assertEqual(bm_dict['name'], "example")
+        self.assertEqual(bm_dict['number'], 42)
 
-    test_to_dict_contains_class_key(self):
+    def test_to_dict_contains_class_key(self):
+        """
+            Test that dictionary contains __class__ key
+        """
         bm = BaseModel()
 
         bm_dict = bm.to_dict()
@@ -47,7 +56,11 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue("__class__" in bm_dict)
         self.assertEqual(bm_dict['__class__'], 'BaseModel')
 
-    test_to_dict_iso_format(self):
+    def test_to_dict_iso_format(self):
+        """
+            Test that the to_dict method converts created_at and
+            update_at to string object in ISO format.
+        """
         bm = BaseModel()
 
         bm_dict = bm.to_dict()
