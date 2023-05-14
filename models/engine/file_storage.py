9f9246c16bcfc6cd7daaea52ac4d1a1bafb3e144
Omar Jammeh <orms45@gmail.com>
2023-05-14 04:18:18 +0000

    Documentation fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,37 +11,32 @@ from models.review import Review
 
 
 class FileStorage:
-    """
-    Serializes instances to a JSON file
-    and deserializes JSON file to instances
+    """Serializes instances to a JSON file and
+    deserializes JSON file to instances
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        returns the dictionary __objects
+        """returns the dictionary __objects
         """
         return self.__objects
 
     def new(self, obj):
-        """
-        Sets in __objects the obj with key <obj class name>.id
+        """Sets in __objects the obj with key <obj class name>.id
         """
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        """
-        Serializes __objects to the JSON file
+        """Serializes __objects to the JSON file
         """
         obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
-        """
-        Deserializes the JSON file to __objects, only if the file pathe exist
+        """Deserializes the JSON file to __objects
         """
         try:
             with open(self.__file_path, encoding='utf-8') as f:
@@ -53,15 +48,13 @@ class FileStorage:
             pass
 
     def destroy(self, key):
-        """
-        Deletes an object from objects
+        """Deletes an object from objects
         """
         self.__objects.pop(key)
         self.save()
 
     def update(self, key, attr, value):
-        """
-        Uses the key to add or update attr value
+        """Uses the key to add or update attr value
         """
         if hasattr(self.__objects[key], attr):
             current_value = getattr(self.__objects[key], attr)
@@ -73,8 +66,7 @@ class FileStorage:
         self.save()
 
     def ids(self):
-        """
-        Returns a dictionary of class names and ids
+        """Returns a dictionary of class names and ids
         """
         object_ids = []
         objects = self.__objects
